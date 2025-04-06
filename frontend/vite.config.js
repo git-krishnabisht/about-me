9188d79308b5254d786eab67b0566dcce0bdb82f
krish <krishnaasinghbisht@gmail.com>
2025-04-06 22:34:34 +0530

    commit-2 changes: configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [
-    tailwindcss(),
-    react()
+    react(), 
+    tailwindcss()
   ],
-})
+  server: {
+    port: 5000,
+  },
+  build: {
+    outDir: 'public',
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 1000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
