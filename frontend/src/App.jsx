d3007cd073eb8db85ca8552ae0fe8395133160b8
krish <krishnaasinghbisht@gmail.com>
2025-05-04 17:16:06 +0530

    commit-12 change: App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -464,7 +464,7 @@ function ProjectsSection() {
   ];
 
   return (
-    <section id="projects" className="py-16 px-4 md:px-20 lg:px-40">
+    <section id="projects" className="py-16 px-4 md:px-20 lg:px-40 ">
       <h2 className="text-3xl md:text-4xl font-bold mb-8 border-b-2 border-blue-200 pb-2">
         Projects
       </h2>
@@ -561,7 +561,7 @@ function ExperienceItem({ period, title, company, description }) {
 
 function BlogSection() {
   return (
-    <section id="blog" className="py-16 px-4 md:px-20 lg:px-40 bg-gray-50">
+    <section id="blog" className="py-16 px-4 md:px-20 lg:px-40 ">
       <h2 className="text-3xl md:text-4xl font-bold mb-8 border-b-2 border-blue-200 pb-2">
         Blogs
       </h2>
